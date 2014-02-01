828c84c78a7eb0af60da85c0a69ee4f0f1232c27
Bojan Jordanovski <jordanovskibojan@gmail.com>
2014-02-01 22:00:32 +0100

    Create new features on user dashboard

--- spec/features/user_dashboard_spec.rb
+++ spec/features/user_dashboard_spec.rb
@@ -14,4 +14,17 @@ feature "User Dashboard" do
 		expect(page).to have_content "Welcome, #{user.user_name}"
 	end
 
+	scenario "back to home" do 
+		click_link "Back to home"
+		expect(current_path).to eq root_path
+	end
+
+	scenario "dashboard need to have user's details" do
+		expect(page).to have_content("#{user.first_name}")
+		expect(page).to have_content("#{user.last_name}")
+		expect(page).to have_content("#{user.email}")
+		expect(page).to have_content("#{user.mobile_number_1}")
+		expect(page).to have_content("#{user.adverts.count}")
+	end
+
 	end
