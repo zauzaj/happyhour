0d24457243524a0f4dc6d35194a449d5ae00cfeb
Иван Бишевац <ivan.bisevac@gmail.com>
2014-02-13 21:59:42 +0100

    Nivo komit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
 
   def after_sign_in_path_for(resource)
@@ -10,5 +11,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
   	super
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:user_name, :email, :password, :mobile_number_1) }
   end
 end
