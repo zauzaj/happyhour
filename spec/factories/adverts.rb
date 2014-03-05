652623fb3c0797d59a6fc1adaa5087ba09374303
zauzaj <jordanovskibojan@gmail.com>
2014-03-05 22:58:49 +0100

    Create show test

--- spec/factories/adverts.rb
+++ spec/factories/adverts.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
 	factory :advert do 
 		title "Kokteli na snizenju"
-		description "Svi kokteli posle 19h na snizenju od 10%-30%"
+		description "Svi kokteli posle 19h na snizenju od 10%-30%. Za najbrze posetioce 
+								 sledi dodatni poklon. Subotom od 21h miss mokre majice !"
 		address "Cara Dusana 12"
 		location "Zemun"
 		phone_number "11111111"
--- spec/features/advert/advert_spec.rb
+++ spec/features/advert/advert_spec.rb
@@ -2,12 +2,22 @@ require 'spec_helper'
 
 feature Advert do
 	let!(:advert) { create(:advert) }
+	
+	def contacts
+		expect(page).to have_content "#{advert.location}"
+		expect(page).to have_content "#{advert.phone_number}"
+		expect(page).to have_content "#{advert.address}"
+	end
 
 	background do
 		visit root_path
 	end
 
 	context "on index page" do
+		background do
+			visit root_path
+		end
+
 		it "should have title" do
 			expect(page).to have_content "#{advert.title}"
 			# save_and_open_page
@@ -18,27 +28,42 @@ feature Advert do
 		end
 
 		it "should have description" do
-			expect(page).to have_content "#{advert.description}"
+			expect(page).to have_content "#{advert.description.truncate 25}"
 		end
 
 		it "should have location,phone number and address" do
-			expect(page).to have_content "#{advert.location}"
-			expect(page).to have_content "#{advert.phone_number}"
-			expect(page).to have_content "#{advert.address}"
+			contacts
 		end
 
 		it "should have link" do
 			expect(page).to have_link "Pogledaj detaljnije.."
 		end
-	end
 
-	context "after clicking on link" do
-		it "should redirect to single advert page" do
-			click_link "Pogledaj detaljnije.."
-			# save_and_open_page
-			expect(current_path).to eq(advert_path(advert))
+		context "after clicking on link" do
+			it "should redirect to single advert page" do
+				click_link "Pogledaj detaljnije.."
+				# save_and_open_page
+				expect(current_path).to eq(advert_path(advert))
+			end
 		end
 	end
+
+	context "on show page" do
+		background do 
+			visit advert_path(advert)
+		end
+
+		it "should have full description" do
+			save_and_open_page
+			expect(page).to have_content "#{advert.description}"
+		end
+
+		it "should have location,phone number and address" do
+			contacts
+		end
+		
+	end
+	
 
 
 end
