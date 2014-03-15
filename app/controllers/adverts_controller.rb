b58dfc7d5b5cf63a46c92cc8adc65a4eb1971148
Bojan Jordanovski <jordanovskibojan@gmail.com>
2014-03-15 12:19:04 +0100

    Create status attribute for adverts

--- app/controllers/adverts_controller.rb
+++ app/controllers/adverts_controller.rb
@@ -4,7 +4,7 @@ class AdvertsController < ApplicationController
   # GET /adverts
   # GET /adverts.json
   def index
-    @adverts = Advert.all
+    @adverts = Advert.active
   end
 
   # GET /adverts/1
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140225214304) do
+ActiveRecord::Schema.define(version: 20140313192430) do
 
   create_table "adverts", force: true do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140225214304) do
     t.integer  "user_id"
     t.integer  "category_id"
     t.boolean  "active",         default: true
+    t.string   "status"
   end
 
   add_index "adverts", ["category_id"], name: "index_adverts_on_category_id", using: :btree
