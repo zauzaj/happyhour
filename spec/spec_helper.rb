2fa54c3378fc5e7ecb290637c2cb9b8280c602c2
Иван Бишевац <ivan.bisevac@gmail.com>
2014-03-27 01:48:52 +0100

    Fix dashboard controller spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,8 +26,11 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+  
   config.include FactoryGirl::Syntax::Methods
+  config.include Devise::TestHelpers, type: :controller
   config.include LoginMacros 
+  
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
