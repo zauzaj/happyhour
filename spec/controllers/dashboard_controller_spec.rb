65b42648e2d396ca1c8b890c1dfeda17ac265aef
Иван Бишевац <ivan.bisevac@gmail.com>
2014-05-07 22:41:18 +0200

    Fix dashboard_controller_spec

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 
 describe DashboardsController do
-
   describe "GET 'index'" do
   	let(:user) { FactoryGirl.create(:user) }
+    
+    # It should be before each since devise session is cleared between tests.
+    before :each do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in(user)
+    end
 
     it "returns http success" do
-    	@request.env["devise.mapping"] = Devise.mappings[:user]
-    	sign_in(user)
       get 'index'
       
       expect(response).to render_template :index
@@ -16,7 +19,8 @@ describe DashboardsController do
     it "assigns all adverts by current_user as @all_my_adverts" do
     	advert = FactoryGirl.create(:active_advert, user: user)
     	get :index
-    	expect(assigns(:all_my_adverts)).to eq([advert])
+    	
+      expect(assigns(:all_my_adverts)).to eq([advert])
     end
   end
 
