1d4b1d1beb3b27696c9c7be12bddc6317e270c9d
Иван Бишевац <ivan.bisevac@gmail.com>
2014-05-17 02:16:01 +0200

    Added feature specs for profile image upload

--- spec/features/user/sign_up_spec.rb
+++ spec/features/user/sign_up_spec.rb
@@ -9,7 +9,6 @@ feature "User - Sign up" do
 			fill_in 'user_password_confirmation', with: "password"		
 			fill_in 'user_email', with: "user@hh.com"
 			fill_in 'user_mobile_number_1', with: "08844838482"
-			# click_button "Upload Avatar"
 			click_button 'Sign up'
 		end
 
@@ -29,5 +28,36 @@ feature "User - Sign up" do
 		end
 
 		expect(current_path).to eq dashboard_path
+	end
+
+	scenario "register with profile image" do
+		visit new_user_registration_path
+
+		within '#new_user' do
+			fill_in 'user_user_name', with: "NewUser"
+			fill_in 'user_password', with: "password"
+			fill_in 'user_password_confirmation', with: "password"		
+			fill_in 'user_email', with: "user@hh.com"
+			fill_in 'user_mobile_number_1', with: "08844838482"
+			attach_file 'user_image', Rails.root.join('spec/fixtures/images/ruby.jpg').to_s
+			click_button 'Sign up'
+		end
+
+		expect(page).to have_css 'img ~ input#user_image'
+	end
+
+	scenario "register without profile image" do
+		visit new_user_registration_path
+
+		within '#new_user' do
+			fill_in 'user_user_name', with: "NewUser"
+			fill_in 'user_password', with: "password"
+			fill_in 'user_password_confirmation', with: "password"		
+			fill_in 'user_email', with: "user@hh.com"
+			fill_in 'user_mobile_number_1', with: "08844838482"
+			click_button 'Sign up'
+		end
+
+		expect(page).not_to have_css 'img ~ input#user_image'
 	end
 end
